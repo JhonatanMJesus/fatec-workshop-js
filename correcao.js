38844c9c6f65dab78fb7421b546c696414d4fc53
Jhonatan Marcelino <134547236+JhonatanMJesus@users.noreply.github.com>
2024-09-09 19:36:08 -0300

    correcao.js

--- correcao.js
+++ correcao.js
@@ -3,9 +3,11 @@
 //DSM - 2º Semestre
 
 function somar() {
-    const y = 5 //Declaração de variável que não pode ter o seu valor alterado.
-    let x = 0; //Declaração de variável que possa ter seu valor alterado.
-    x + 10; //Indica uma soma padrão com a variável "x".
-    let r = x + y; //Declaração da variável "r" para armazenar o valor do resultado da soma das variáveis X e Y.
-    console.log("O resultado da soma é: " + r) //Mostra no console o valor armazenado na variável "r"
+  const y = 5;
+  let x = 5;
+  x = x + 10;
+  let r = x + y;
+  return r;
 }
+
+console.log('O resultado da soma é: ' + somar());
