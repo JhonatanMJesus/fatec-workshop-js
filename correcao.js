1a3bfe971b8e8ef81de1754587851dde926ad671
Jhonatan Marcelino <134547236+JhonatanMJesus@users.noreply.github.com>
2024-09-09 19:39:41 -0300

    correcao.js

--- correcao.js
+++ correcao.js
@@ -3,10 +3,10 @@
 //DSM - 2º Semestre
 
 function somar() {
-  const y = 5;
-  let x = 5;
-  x = x + 10;
-  let r = x + y;
+  const y = 5; //Declarando a variavel y como constante pois nao sofrerá alteração.
+  let x = 5; //Declarando x como let pois só será usada dentro dessa função e poderá sofrer alteração.
+  x = x + 10; //Realiza uma operação de acúmulo na variável x.
+  let r = x + y; //Realiza a soma entre as variaveis x e y e guarda o resultado dentro da variavel r
   return r;
 }
 
