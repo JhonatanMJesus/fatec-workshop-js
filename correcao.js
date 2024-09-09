52e04082a6c7e210fcc6cc4ba2811a01c6f609c3
Jhonatan Marcelino <134547236+JhonatanMJesus@users.noreply.github.com>
2024-09-09 08:28:03 -0300

    Labi9 - Fatec Jahu Workshop JS
    Correção do desafio da Labi9 - Fatec Jahu Workshop JS

---
+++ correcao.js
@@ -0,0 +1,11 @@
+//Correção do código - Fatec Jahu Workshop JS
+//Nome: Jhonatan MArcelino de Jesus
+//DSM - 2º Semestre
+
+function somar() {
+    const y = 5 //Declaração de variável que não pode ter o seu valor alterado.
+    let x = 0; //Declaração de variável que possa ter seu valor alterado.
+    x + 10; //Indica uma soma padrão com a variável "x".
+    let r = x + y; //Declaração da variável "r" para armazenar o valor do resultado da soma das variáveis X e Y.
+    console.log("O resultado da soma é: " + r) //Mostra no console o valor armazenado na variável "r"
+}
